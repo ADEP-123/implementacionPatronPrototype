d7ba3d5bb37982d988c25799bc4c2fd3a5dcbfd6
ADEP-123 <andep210399@gmail.com>
2024-10-05 17:29:01 -0500

    feat: :sparkles: interfaz documento con los metodos clonar y editar contenido

---
+++ src/documents/Documento.js
@@ -0,0 +1,11 @@
+class Documento {
+    clonar() {
+        throw new Error('Este método debe ser implementado');
+    }
+
+    editarContenido(nuevoContenido) {
+        throw new Error('Este método debe ser implementado');
+    }
+}
+
+export default Documento;
